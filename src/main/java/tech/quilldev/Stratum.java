69b2fa5ebc6af7b45f625c6a724634d6d1985258
Robert Brunson <siinspam@gmail.com>
2021-04-28 14:58:55 -0400

    a new world

--- src/main/java/tech/quilldev/Stratum.java
+++ src/main/java/tech/quilldev/Stratum.java
@@ -1,19 +1,12 @@
 package tech.quilldev;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import tech.quilldev.Commands.ChangeServer;
-import tech.quilldev.Commands.Lobby;
-import tech.quilldev.Commands.NewWorld;
+
 import tech.quilldev.Events.onJoin;
 import tech.quilldev.Events.onLeave;
 import tech.quilldev.Events.onMessage;
-import tech.quilldev.Helpers.ConfigManager;
-
-import java.util.Objects;
 
 public final class Stratum extends JavaPlugin {
-
-    private final ConfigManager configManager = new ConfigManager(this);
 
     @Override
     public void onEnable() {
@@ -23,18 +16,10 @@ public final class Stratum extends JavaPlugin {
         pluginManager.registerEvents(new onJoin(), this);
         pluginManager.registerEvents(new onMessage(), this);
         pluginManager.registerEvents(new onLeave(), this);
-
-        var changeServer = new ChangeServer(configManager);
-
-        //Add executable commands
-        Objects.requireNonNull(this.getCommand("newworld")).setExecutor(new NewWorld(configManager));
-        Objects.requireNonNull(this.getCommand("changeserver")).setExecutor(changeServer);
-        Objects.requireNonNull(this.getCommand("lobby")).setExecutor(new Lobby(changeServer));
     }
 
     @Override
     public void onDisable() {
-        configManager.save();
     }
 
 }
